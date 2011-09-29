25ccfd128682a41599d218179d22a566ed27ab41
boris <github.com@ll.glass.li>
2011-09-29 21:27:31 +0200

    recursive display of phases/milestones/deliverables

--- app/models/milestone.rb
+++ app/models/milestone.rb
@@ -1,2 +1,6 @@
 class Milestone < ActiveRecord::Base
+  belongs_to   :phase
+  has_many     :deliverables
+  has_many     :payments
+  has_many     :meetings
 end
--- app/models/phase.rb
+++ app/models/phase.rb
@@ -1,2 +1,3 @@
 class Phase < ActiveRecord::Base
+  has_many :milestones
 end
