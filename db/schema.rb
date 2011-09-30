9628562c97582d9e579fbcec658b44ed951e14d7
Boris Glass <glass@puck.(none)>
2011-09-30 17:30:22 +0200

    css improved

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110928213338) do
+ActiveRecord::Schema.define(:version => 20110928214620) do
 
   create_table "actions", :force => true do |t|
     t.integer  "activity_id"
